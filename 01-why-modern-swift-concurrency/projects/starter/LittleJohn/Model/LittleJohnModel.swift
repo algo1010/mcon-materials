5f360dba58026f81f2b55db162ca621b4160ee7e
datdn <datdn.ios@gmail.com>
2023-03-22 17:09:45 +0700

    introduce async, await and async sequence

--- 01-why-modern-swift-concurrency/projects/starter/LittleJohn/Model/LittleJohnModel.swift
+++ 01-why-modern-swift-concurrency/projects/starter/LittleJohn/Model/LittleJohnModel.swift
@@ -45,6 +45,35 @@ class LittleJohnModel: ObservableObject {
     guard let url = URL(string: "http://localhost:8080/littlejohn/ticker?\(selectedSymbols.joined(separator: ","))") else {
       throw "The URL could not be created."
     }
+    
+    let (stream, response) = try await liveURLSession.bytes(from: url)
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+      throw "The server responded with an error."
+    }
+    for try await line in stream.lines {
+      let sortedSymbols = try JSONDecoder()
+        .decode([Stock].self, from: Data(line.utf8))
+        .sorted { $0.name < $1.name }
+      await MainActor.run {
+        tickerSymbols = sortedSymbols
+      }
+    }
+    
+    // reset view to empty when server error
+    await MainActor.run {
+      tickerSymbols = []
+    }
+  }
+  
+  func avaiableSymbols() async throws -> [String] {
+    guard let url = URL(string: "http://localhost:8080/littlejohn/symbols") else {
+      throw "The URL could not be created."
+    }
+    let (data, response) = try await liveURLSession.data(for: URLRequest(url: url))
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+      throw "The server responded with an error."
+    }
+    return try JSONDecoder().decode([String].self, from: data)
   }
 
   /// A URL session that lets requests run indefinitely so we can receive live updates from server.
